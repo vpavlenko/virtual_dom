9942ee8971446c856348622973463e6ece94eb63
Vitaly Pavlenko <cxielamiko@gmail.com>
2019-02-14 23:19:53 +0300

    Start new implementation with h/mount/patch. Issue: what to do with text nodes?

--- virtual_dom.js
+++ virtual_dom.js
@@ -1,27 +1,71 @@
-class Component {
-  constructor() {
-    console.log('constructor called');
-  }
+(() => {
 
-
-}
-
-const createElement = (component, props, content) => {
-  if (typeof component === "string") {
-    const element = document.createElement(component);
-    element.appendChild(document.createTextNode(content));
-    return element;
-  }
+const h = (type, props, children) => {
+  return {
+    type,
+    props,
+    children,  // always an array
+  };
 };
 
-const render = (element, container) => {
+const mount = (virtualNode) => {
   debugger;
-  container.innerHTML = '';
-  container.appendChild(element);
+  // When a child is a string
+  if (typeof virtualNode === "string") {
+    return document.createTextNode(virtualNode);
+  }
+  const { type, props, children } = virtualNode;
+  const realNode = document.createElement(type);
+  virtualNode._realNode = realNode;
+  for (const key in props) {
+    realNode.setAttribute(key, props[key]);
+  }
+  if (children) {
+    for (const child of children) {
+      realNode.appendChild(mount(child));
+    }
+  }
+  return realNode;
+};
+
+const patch = (newVirtualNode, oldVirtualNode, parent) => {
+  debugger;
+  if (oldVirtualNode === newVirtualNode) {
+    return;
+  }
+  if (!oldVirtualNode) {
+    parent.appendChild(mount(newVirtualNode));
+  } else if (!newVirtualNode) {
+    parent.removeChild(oldVirtualNode._realNode);
+  } else {
+    if ()
+    const realNode = oldVirtualNode._realNode;
+    if (newVirtualNode.type !== oldVirtualNode.type) {
+      parent.replaceChild(realNode, mount(newVirtualNode));
+    }
+    for (const key in newVirtualNode.props) {
+      if (newVirtualNode.props[key] !== oldVirtualNode.props[key]) {
+        realNode.setAttribute(key, newVirtualNode.props[key]);
+      }
+    }
+    for (let i = 0; i < oldVirtualNode.children.length; ++i) {
+      if (i < newVirtualNode.children.length) {
+        patch(newVirtualNode.children[i], oldVirtualNode.children[i], realNode);
+      } else {
+        realNode.removeChild(oldVirtualNode.children[i]._realNode);
+      }
+    }
+    for (let i = oldVirtualNode.children.length; i < newVirtualNode.children.length; ++i) {
+      realNode.appendChild(mount(newVirtualNode.children[i]));
+    }
+  }
+
 };
 
 window.VirtualDOM = {
-  Component,
-  createElement,
-  render,
+  h,
+  mount,
+  patch,
 };
+
+})();
